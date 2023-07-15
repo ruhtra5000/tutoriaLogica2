00af87afa52044c8d6a29a684154ad0756890722
Arthur de Sá Tenório <60717938+ruhtra5000@users.noreply.github.com>
2023-07-15 16:06:07 -0300

    Update questao2.c

--- questao2.c
+++ questao2.c
@@ -1,3 +1,17 @@
+/*
+Primeiramente, MMC e MDC não incluem o número 0, já que 0 é múltiplo de todos os números inteiros e não é divisor de nenhum deles. 
+
+Segundamente, números negativos não possuem MMC, porque não importa qual número é o suposto menor múltiplo de um número negativo, 
+já que sempre haverá um menor do que ele que também é múltiplo do par de números escolhido.
+
+Em relação à lógica por trás dos cálculos: o MMC é sempre maior ou igual ao maior número digitado, então, partindo dele de forma ascendente, 
+o primeiro número encontrado que seja múltiplo do maior e menor número digitado será o seu MMC. Já o MDC sempre será menor ou igual ao menor 
+número digitado, então, partindo dele de forma descendente, o primeiro número que divide o par de números digitados será o seu MDC (este sempre 
+será, no mínimo, 1), e usá-se o módulo dos valores digitados, pois o MDC de qualquer número negativo é igual ao MDC do seu antípoda.
+
+Considerando o conjunto dos números inteiros como sendo o domínio desta questão, eu acredito que todos eles estão contemplados neste programa, tendo ou não MMC e MDC.
+*/
+
 #include <stdio.h>
 #include <math.h>
 
